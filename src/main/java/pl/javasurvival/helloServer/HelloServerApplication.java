9ed57ed3d36e4995514e1c521ee01bdf37c9aa2c
Jarek Ratajski <jratajski@gmail.com>
2017-12-03 07:24:48 -0800

    Lekcja 3 : po liczniku

--- src/main/java/pl/javasurvival/helloServer/HelloServerApplication.java
+++ src/main/java/pl/javasurvival/helloServer/HelloServerApplication.java
@@ -9,21 +9,26 @@ import reactor.ipc.netty.http.server.HttpServer;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 public class HelloServerApplication {
+    static AtomicInteger counter = new AtomicInteger(0);
 
 	public static void main(String[] args) {
+
 		RouterFunction route = route( GET("/"),
 				request -> {
-				String welcome = "Witaj na stronie obozu przetrwania";
+		        String welcome = "Witaj na stronie obozu przetrwania";
 				LocalDateTime now = LocalDateTime.now();
 				DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 				String time = "\nCzas to:"+ now.format(myFormatter);
-				return ServerResponse.ok().body(fromObject(welcome + time));
+				String visits = "\nTo są "+ counter.incrementAndGet() + " odwiedziny";
+
+				return ServerResponse.ok().body(fromObject(welcome + time + visits));
 			});
 
 		HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
