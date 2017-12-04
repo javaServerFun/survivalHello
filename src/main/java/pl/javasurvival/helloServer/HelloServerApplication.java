369623d2888027db5d9dff38d0ecc53a2d6158bd
Jarek Ratajski <jratajski@gmail.com>
2017-12-04 02:31:08 -0800

    Lekcja 5 : Koniec

--- src/main/java/pl/javasurvival/helloServer/HelloServerApplication.java
+++ src/main/java/pl/javasurvival/helloServer/HelloServerApplication.java
@@ -7,27 +7,40 @@ import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
 
-import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 public class HelloServerApplication {
+    private final List<Message> messages = new ArrayList<>();
+
+
+    private HelloServerApplication() {
+        messages.add( new Message("test content", "Zenek Testowy"));
+        messages.add( new Message("bla bla", "Sebastian Tester"));
+    }
+
     public static void main(String[] args) {
         new HelloServerApplication().serve();
 
     }
 
     private void serve() {
-        RouterFunction route = route(GET("/api/time"),
-                renderWelcome());
+        RouterFunction route = nest( path("/api"),
+                route(GET("/time"), renderTime())
+                .andRoute(GET("/messages"), renderMessages())
+                .andRoute(POST("/messages"), postMessage()));
 
         HttpHandler httpHandler = RouterFunctions.toHttpHandler(route);
         HttpServer server = HttpServer.create("localhost", 8080);
@@ -35,7 +48,28 @@ public class HelloServerApplication {
         server.startAndAwait(myReactorHandler);
     }
 
-    private HandlerFunction<ServerResponse> renderWelcome() {
+    private HandlerFunction<ServerResponse> postMessage() {
+        return request -> {
+            Mono<Message> postedMessage = request.bodyToMono(Message.class);
+            return postedMessage.flatMap( message -> {
+                messages.add(message);
+                return ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromObject(messages));
+            });
+
+        };
+    }
+
+    private HandlerFunction<ServerResponse> renderMessages() {
+        return request -> {
+            return ServerResponse.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fromObject(messages));
+        };
+    }
+
+    private HandlerFunction<ServerResponse> renderTime() {
         return request -> {
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
---
+++ src/main/java/pl/javasurvival/helloServer/Message.java
@@ -0,0 +1,16 @@
+package pl.javasurvival.helloServer;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Message {
+    public final String content;
+    public final String author;
+
+    @JsonCreator
+    public Message(@JsonProperty("content") String content,
+                   @JsonProperty("author") String author) {
+        this.content = content;
+        this.author = author;
+    }
+}
