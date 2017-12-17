5ae42b6be7fa000944514367b4571d0d3fcaf9e9
Jarek Ratajski <jratajski@gmail.com>
2017-12-17 17:51:15 +0100

    LEKCJA10: MongoDB added - better sync

--- src/main/java/pl/javasurvival/helloServer/MessageBoardService.java
+++ src/main/java/pl/javasurvival/helloServer/MessageBoardService.java
@@ -25,15 +25,28 @@ public class MessageBoardService {
                                                 .getOrElse(Topic.create(newElement.topic))
                                                 .addMessage(newElement.message))
                 );
+        ensureTopic("test");
+        ensureTopic("general");
+        ensureTopic("java");
+    }
+
+    private void ensureTopic(String test) {
+        this.topics = this.topics.computeIfAbsent(test, key -> Topic.create(key))._2;
     }
 
     synchronized Option<Topic> getTopic(String topicName) {
         return this.topics.get(topicName);
     }
 
-    synchronized Option<Topic> addMessageToTopic(String topicName, Message newMsg) {
-        Option<Topic> newTopic = getTopic(topicName).map(topic -> topic.addMessage(newMsg));
-        newTopic.forEach( topic -> mongoRepository.save(new BoardMessage(newMsg, topicName)));
+    Option<Topic> addMessageToTopic(String topicName, Message newMsg) {
+        Option<Topic> existingTopic = getTopic(topicName);
+        existingTopic.forEach( topic -> mongoRepository.save(new BoardMessage(newMsg, topicName)));
+        Option<Topic> newTopic = updateTopic(topicName, newMsg, existingTopic);
+        return newTopic;
+    }
+
+    private synchronized Option<Topic> updateTopic(String topicName, Message newMsg, Option<Topic> existingTopic) {
+        Option<Topic> newTopic = existingTopic.map(topic -> topic.addMessage(newMsg));
         Option<Map<String, Topic>> newTopics = newTopic.map(topic -> this.topics.put(topicName, topic));
         newTopics.forEach( topics -> this.topics = topics );
         return newTopic;
